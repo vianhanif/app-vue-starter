7c33af8ca278303301b85f9ca966691101c28060
Alvian Rahman Hanif <alvian524@gmail.com>
2017-03-27 11:43:26 +0700

    vue learning part-1, create list, bind input change

--- src/components/Hello/app.vue
+++ src/components/Hello/app.vue
@@ -2,22 +2,19 @@
 <script src="./app.js"/>
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 v-on:click="reverseText" v-bind:title="time_message">{{ msg }} {{ name }}</h1>
+    <input v-model="name"/>
     <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    <ul v-if="seen">
+      <li v-for="link in links.essential">
+        <a v-bind:href="link.href" target="_blank">{{link.text}} <slot></slot>    </a>
+      </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="link in links.ecosystem">
+        <a v-bind:href="link.href" target="_blank">{{link.text}}<slot></slot></a>
+      </li>
     </ul>
   </div>
 </template>
