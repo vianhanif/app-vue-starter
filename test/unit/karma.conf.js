18fa2d4d9aec845849d4da92b3dcc1c4bfe18c68
Alvian Rahman Hanif <alvian524@gmail.com>
2017-03-27 10:31:25 +0700

    change browser for testing to use Chrome, Firefox, Safari

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,23 +11,17 @@ module.exports = function (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome', 'Firefox', 'Safari'],
     frameworks: ['mocha', 'sinon-chai'],
-    reporters: ['spec', 'coverage'],
+    reporters: ['mocha'],
     files: ['./index.js'],
+    logLevel: config.LOG_WARN,
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true
-    },
-    coverageReporter: {
-      dir: './coverage',
-      reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' }
-      ]
     }
   })
 }
