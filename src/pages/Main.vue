a6ac415c4985e2966c0d816ad84300e2546bbc83
Alvian Rahman Hanif <alvian524@gmail.com>
2017-08-10 16:57:49 +0700

    updates

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,23 +1,6 @@
 <template lang="html">
   <v-app id="example-2" standalone>
     <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar tag="div">
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>John Leider</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-btn icon @click.native.stop="mini = !mini">
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
       <v-list class="pt-0" dense>
         <v-divider></v-divider>
         <v-list-tile v-for="item in items" :key="item.title">
@@ -32,7 +15,7 @@
     </v-navigation-drawer>
     <v-toolbar fixed class="indigo darken-4" dark>
       <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-toolbar>
     <main>
       <v-container fluid>
