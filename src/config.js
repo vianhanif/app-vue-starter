2178f9247a88c9f36472503f262e09163cf7231c
Alvian Rahman Hanif <alvian524@gmail.com>
2017-10-01 15:02:19 +0700

    build

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { encrypt } from '@/crypto'
 
 export default {
   app: {
-    title: 'Collision Tester - Scytale(RailFence(Jefferson(DATA)))',
+    title: 'Collision Tester - RailFence(Jefferson(Scytale(DATA)))',
     SHA (text) {
       return encrypt([
         'jefferson',
