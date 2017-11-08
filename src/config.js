001b8adc869a762feb8091749a8d67283df10ea3
Alvian Rahman Hanif <alvian524@gmail.com>
2017-11-08 15:58:39 +0700

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { encrypt } from '@/crypto'
 
 export default {
   app: {
-    title: 'Collision Tester (Updated) - SJR(DATA)',
+    title: 'Collision Tester ( Updated ) - SJR(DATA)',
     SHA (text) {
       return encrypt([
         'scytale',
