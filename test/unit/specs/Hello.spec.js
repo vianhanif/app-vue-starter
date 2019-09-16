12d3fe32f7dea8a3103d3f49b85e11baabc1c5e2
Alvian Rahman Hanif <alvian524@gmail.com>
2019-09-16 15:25:09 +0700

    fix test

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import Hello from '@/components/Hello/app'
 
 describe('Hello/app.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Hello)
-    const vm = new Constructor().$mount()
+    const vm = new Constructor({
+      router: new Router({}),
+    }).$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App ')
   })
